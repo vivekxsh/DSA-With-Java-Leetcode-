cf87842d06bec8c61b72b7c1a8e5ca00a22479bb
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-07-02 00:52:00 +0530

    Time: 268 ms (5.01%) | Memory: 45.5 MB (62.03%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        
+        ArrayList<Integer> helper = new ArrayList<>();
+        int repeated = -1;
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+
+                if(!(helper.contains(grid[i][j]))){
+                    
+                    helper.add(grid[i][j]);
+                }
+                else{
+                    repeated = grid[i][j];
+                }
+
+                
+
+            }
+        }
+
+        Collections.sort(helper);
+
+        int missing = -1;
+        int start = 1;
+        for(int i=0; i<helper.size(); i++){
+            if(helper.get(i) == start){
+                start++;
+            }
+            else{
+                missing = start;
+            }
+        }
+
+        if(missing == -1){
+            missing = helper.get(helper.size()-1) + 1;
+        }
+
+        return new int[]{repeated, missing};
+
+    }
+}
