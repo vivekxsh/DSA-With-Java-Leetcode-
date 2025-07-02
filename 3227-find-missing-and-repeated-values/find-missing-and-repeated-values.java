4cd23260a352641b752f953f268295ed1f2598d6
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-07-03 01:33:00 +0530

    Time: 265 ms (5.27%) | Memory: 45.8 MB (26.16%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,31 +1,27 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         
+
         ArrayList<Integer> helper = new ArrayList<>();
         int repeated = -1;
 
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-
-                if(!(helper.contains(grid[i][j]))){
-                    
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(!helper.contains(grid[i][j])) {
                     helper.add(grid[i][j]);
                 }
                 else{
                     repeated = grid[i][j];
                 }
-
-                
-
             }
         }
 
         Collections.sort(helper);
-
         int missing = -1;
         int start = 1;
-        for(int i=0; i<helper.size(); i++){
-            if(helper.get(i) == start){
+
+        for(int num : helper) {
+            if(num == start) {
                 start++;
             }
             else{
@@ -33,7 +29,7 @@ class Solution {
             }
         }
 
-        if(missing == -1){
+        if(missing == -1) {
             missing = helper.get(helper.size()-1) + 1;
         }
 
