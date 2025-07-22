71b424b1d96ae0859c1c0137fd99df80474ce01c
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-07-22 12:02:59 +0530

    Time: 5 ms (99.47%) | Memory: 58.6 MB (76.25%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.java
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -1,34 +1,60 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
 
-        if(nums == null || nums.length == 0) {
-            return 0;
-        }
+        // if(nums == null || nums.length == 0) {
+        //     return 0;
+        // }
 
-        int sum = 0;
+        // int sum = 0;
 
-        HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> set = new HashSet<>();
 
+        // int left = 0;
+
+        // for(int right = 0; right < nums.length; right++) {
+
+        //     while(set.contains(nums[right])) {
+        //         set.remove(nums[left]);
+        //         left++;
+        //     }
+
+        //     set.add(nums[right]);
+
+        //     int tempSum = 0;
+        //     for(int i = left; i<=right; i++) {
+        //         tempSum += nums[i];
+        //     }
+
+        //     sum = Math.max(tempSum, sum);
+        // }
+
+        // return sum;
+
+
+
+        int n = nums.length;
+
+        boolean isSeen[] = new boolean[10001];
         int left = 0;
+        int currentSum = 0;
+        int maxSum = 0;
 
-        for(int right = 0; right < nums.length; right++) {
+        for(int right = 0; right < n; right++) {
 
-            while(set.contains(nums[right])) {
-                set.remove(nums[left]);
+            while(isSeen[nums[right]]) {
+                currentSum -= nums[left];
+                isSeen[nums[left]] = false;
                 left++;
             }
 
-            set.add(nums[right]);
-
-            int tempSum = 0;
-            for(int i = left; i<=right; i++) {
-                tempSum += nums[i];
+            currentSum += nums[right];
+            isSeen[nums[right]] = true;
+            if(currentSum > maxSum) {
+                maxSum = currentSum;
             }
-
-            sum = Math.max(tempSum, sum);
         }
 
-        return sum;
+        return maxSum;
 
         
     }
