d8ed93cc56c952d58e88844d01f2a1c0a479e8a1
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-08-11 23:02:22 +0530

    Time: 17 ms (17.78%) | Memory: 44.6 MB (50.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
+        if(s.length() != t.length()){
+            return false;
+        }
+
         HashMap<Character, Integer> map = new HashMap<>();
 
         for(int i=0; i<s.length(); i++) {
