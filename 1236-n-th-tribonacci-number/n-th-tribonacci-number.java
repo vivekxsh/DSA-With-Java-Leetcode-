b7715a52e80babbdc1ffcd29fad389956785f9e6
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-08-29 10:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (92.25%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int tribonacci(int n) {
 
+        // ******* By using Recursion *******
         // if(n==0 || n==1){
         //     return n;
         // }
@@ -11,8 +12,32 @@ class Solution {
 
         // return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
 
-        if(n <= 2){
-            if(n==2){
+
+        // ***** By using DP [Tabulation method] *****
+        // if(n <= 2){
+        //     if(n==2){
+        //         return 1;
+        //     }
+        //     else{
+        //         return n;
+        //     }
+        // }
+
+        // int dp[] = new int[n+1];
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] = 1;
+
+        // for(int i=3; i<=n; i++) {
+        //     dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        // }
+
+        // return dp[n];
+
+
+        // ****** Normal Swapping technique *****
+        if(n <= 2) {
+            if(n==2) {
                 return 1;
             }
             else{
@@ -20,16 +45,20 @@ class Solution {
             }
         }
 
-        int dp[] = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int prev1 = 0;
+        int prev2 = 1;
+        int prev3 = 1;
 
         for(int i=3; i<=n; i++) {
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+            int fib = prev1+prev2+prev3;
+
+            int temp = prev1;
+            prev1 = prev2;
+            prev2 = prev3;
+            prev3 = fib;
         }
 
-        return dp[n];
+        return prev3;
         
     }
 }
