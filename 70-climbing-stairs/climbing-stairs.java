95a18bedcf6d8d04af926783a1034cc70f06c2e5
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-08-30 11:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (92.74%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        // ***** Recursion technique (Got TLE) *****
+        // if(n==1 || n==2) {
+        //     return n;
+        // }
+
+        // return climbStairs(n-1)+climbStairs(n-2);
+
+
+        // ***** Using DP [Tabulation method] *****
+        if(n <= 2) {
+            return n;
+        }
+
+        int dp[] = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+
+    }
+}
