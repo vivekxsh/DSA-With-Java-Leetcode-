3dbff2f9533c091bbd24d2d1bd90f2ca59a045de
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2025-09-05 00:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (53.27%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if(s.length() != goal.length()) {
+            return false;
+        }
+
+        s = s+s;
+
+        return s.indexOf(goal)>=0;
+        
+    }
+}
