590beff8df20144a458a9a7289a43e0e25302637
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-04-20 00:53:54 +0530

    Time: 1 ms (99.98%) | Memory: 46.7 MB (84.81%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int xor = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            xor = xor ^ nums[i];
+        }
+
+        return xor;
+        
+    }
+}
