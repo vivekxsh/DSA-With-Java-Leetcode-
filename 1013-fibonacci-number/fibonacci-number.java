cc6d9c52e3d1c70ab7073e8f48c118b869512742
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-07-24 14:58:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (33.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,44 +1,19 @@
 class Solution {
     public int fib(int n) {
-        // if(n == 0 || n == 1) {
-        //     return n;
-        // }
 
-        // return fib(n-1) + fib(n-2);
-
-
-        // using dp (tabulation)
-
-        // if(n==0 || n==1){
-        //     return n;
-        // }
-        // int dp[] = new int[n+1];
-        // dp[0] = 0;
-        // dp[1] = 1;
-
-        // for(int i=2; i<=n; i++) {
-        //     dp[i] = dp[i-1] + dp[i-2];
-        // }
-
-        // return dp[n];
-
-
-        // without dp
-
-        if(n<=1){
+        if(n <= 1) {
             return n;
         }
-        int fib = 0;
-        int prev1 = 0;
-        int prev2 = 1;
-        for(int i=2; i<=n; i++) {
-            fib = prev1 + prev2;
 
-            int temp = prev1;
-            prev1 = prev2;
-            prev2 = fib;
+        int f[] = new int[n+1];
+        f[0] = 0;
+        f[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            f[i] = f[i-1] + f[i-2];
         }
 
-        return fib;
+        return f[n];
+        
     }
 }
