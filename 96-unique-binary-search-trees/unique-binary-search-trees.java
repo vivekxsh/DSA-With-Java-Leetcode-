a21cd977a11783f17d926717991aecc1af6c6ef1
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-08-15 12:54:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (31.29%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,9 +1,25 @@
 class Solution {
     public int numTrees(int n) {
                 
-        int dp[] = new int[n+1];
+        // int dp[] = new int[n+1];
 
-        return catalanVariation(n, dp);
+        // return catalanVariation(n, dp);
+
+        if(n==0 || n==1) {
+            return 1;
+        }
+
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            for(int j=0; j<i; j++) {
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+
+        return dp[n];
 
     }
 
