0212309c76520ed15e375a6c8095ccf3f1b08b41
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-08-26 11:09:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (33.43%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -14,13 +14,26 @@ class Solution {
 
 
         // memoization
+        // if(n==0 || n==1) {
+        //     return n;
+        // }
+        // int dp[] = new int[n+1];
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // return helper(n, dp);
+
+        // tabulation
         if(n==0 || n==1) {
             return n;
         }
         int dp[] = new int[n+1];
         dp[0] = 0;
         dp[1] = 1;
-        return helper(n, dp);
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 
     public int helper(int n, int[] dp) {
