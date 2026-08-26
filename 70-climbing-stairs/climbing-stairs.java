f2ab7d0cd59ec72610b68d40f6f57ce52c26935f
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-08-26 11:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (17.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,36 +1,39 @@
 class Solution {
-
-    public int memo(int n, int[] dp) {
-        if(n<=2) {
-            return n;
-        }
-
-        if(dp[n] != 0) {
-            return dp[n];
-        }
-
-        dp[n] = memo(n-1, dp) + memo(n-2, dp);
-
-        return dp[n];
-    }
-
     public int climbStairs(int n) {
 
-        if(n<=2) {
+        // recursion -> got tle
+        // if(n == 1 || n == 2) {
+        //     return n;
+        // }
+
+        // return climbStairs(n-1)+climbStairs(n-2);
+
+        // memoization
+
+        if(n==1 || n==2) {
             return n;
         }
-
         int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
         dp[1] = 1;
         dp[2] = 2;
 
-        // for(int i=3; i<=n; i++) {
-        //     dp[i] = dp[i-1]+dp[i-2];
-        // }
-
-        // return dp[n];
+        return helper(n, dp);
         
+    }
 
-        return memo(n, dp);
+    public int helper(int n, int[] dp) {
+
+        if(n == 1 || n==2) {
+            return n;
+        }
+
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = helper(n-1, dp) + helper(n-2, dp);
+
+        return dp[n];
     }
 }
