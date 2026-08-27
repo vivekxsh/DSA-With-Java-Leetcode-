f81fdac6e563e67b634b27b22fa55e7be372dafb
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-08-27 09:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.01%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,30 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-
-        // recursion -> got tle
-        // if(n == 1 || n == 2) {
-        //     return n;
-        // }
-
-        // return climbStairs(n-1)+climbStairs(n-2);
-
-        // memoization
-
-        if(n==1 || n==2) {
-            return n;
-        }
+        
         int dp[] = new int[n+1];
+
         Arrays.fill(dp, -1);
-        dp[1] = 1;
-        dp[2] = 2;
 
         return helper(n, dp);
-        
+
     }
 
-    public int helper(int n, int[] dp) {
+    private int helper(int n, int dp[]) {
 
-        if(n == 1 || n==2) {
+        if(n == 1 || n == 2) {
             return n;
         }
 
@@ -35,5 +22,6 @@ class Solution {
         dp[n] = helper(n-1, dp) + helper(n-2, dp);
 
         return dp[n];
+
     }
 }
