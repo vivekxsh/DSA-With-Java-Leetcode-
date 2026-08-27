1233ee51e88817c0ea5cf151be94b8bae98d9372
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-08-27 08:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (96.19%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -3,13 +3,12 @@ class Solution {
 
         int dp[][] = new int[m+1][n+1];
 
+        dp[1][1] = 1;
         for(int i=1; i<=m; i++) {
             for(int j=1; j<=n; j++) {
                 if(dp[i-1][j] == 0 && dp[i][j-1] == 0) {
-                    dp[i][j] = 1;
                     continue;
                 }
-
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
