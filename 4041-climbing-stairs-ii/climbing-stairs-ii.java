c8aa6cbdbc0af85722f969a9a143a5d470fe2767
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-08-27 09:55:33 +0530

    Time: 5 ms (86.92%) | Memory: 117.1 MB (64.01%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -2,22 +2,24 @@ class Solution {
     public int climbStairs(int n, int[] costs) {
         
         int dp[] = new int[n+1];
-
+        
         dp[0] = 0;
+
         dp[1] = cost(costs, 0, 1);
 
-        if(n == 1) {
+        if(n <= 1) {
             return dp[1];
         }
 
-        dp[2] = Math.min(dp[1] + cost(costs, 1,2), cost(costs, 0,2));
+        dp[2] = Math.min(dp[1] + cost(costs, 1, 2), cost(costs, 0, 2));
 
         for(int i=3; i<=n; i++) {
-            int c1 = cost(costs, i-1, i);
-            int c2 = cost(costs, i-2, i);
-            int c3 = cost(costs, i-3, i);
 
-            dp[i] = Math.min(dp[i-1]+c1, Math.min(dp[i-2]+c2, dp[i-3]+c3));
+            int cost1 = cost(costs, i-1, i);
+            int cost2 = cost(costs, i-2, i);
+            int cost3 = cost(costs, i-3, i);
+
+            dp[i] = Math.min(cost1+dp[i-1], Math.min(cost2+dp[i-2], cost3+dp[i-3]));
 
         }
 
@@ -25,7 +27,7 @@ class Solution {
 
     }
 
-    private int cost(int[] costs, int i, int j) {
-        return costs[j-1]+(j-i)*(j-i);
+    private int cost(int costs[], int i, int j) {
+        return costs[j-1] + (j-i)*(j-i);
     }
 }
