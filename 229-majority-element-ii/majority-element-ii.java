46b7d66c290fdaed653e603a207efd6d600e0830
Vivek Kumar <118986548+vivekxsh@users.noreply.github.com>
2026-09-03 14:26:40 +0530

    Time: 14 ms (43.34%) | Memory: 51.5 MB (87.98%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        
+
         List<Integer> ans = new ArrayList<>();
 
-        Map<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=0; i<nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            map.put(nums[i], map.getOrDefault(nums[i] , 0) + 1);
         }
 
         for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
@@ -16,6 +16,6 @@ class Solution {
         }
 
         return ans;
-
+        
     }
 }
